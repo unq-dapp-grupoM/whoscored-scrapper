597800e15f34a355c84171cadb80de352c69a274
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-10-08 20:19:58 -0300

    Initial commit

---
+++ src/main/java/com/dapp/whoscored_scrapper/WhoscoredScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.dapp.whoscored_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoscoredScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoscoredScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dapp/whoscored_scrapper/WhoscoredScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapp.whoscored_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoscoredScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
